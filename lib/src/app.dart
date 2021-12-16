55e4a7687a8d18b3fc1eb7a6a43c69c8c92e6d67
Jose Alfredo <chpgomezito@gmail.com>
2021-12-16 15:08:22 -0600

    Ya funciona la camara

---
+++ lib/src/app.dart
@@ -0,0 +1,20 @@
+
+import 'package:camara/src/pages/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Camara con Flutter",
+      home: HomeScreen(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.red,
+        
+      ),
+    );
+  }
+}
---
+++ lib/src/pages/home_screen.dart
@@ -0,0 +1,121 @@
+import 'dart:io';
+
+import 'package:camara/src/pages/image_view.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'camera_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  
+  final ImagePicker? _image  = ImagePicker();
+  //late List<PickedFile> images = [];
+   late List<XFile> images = [];
+   XFile ? file;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cámara con Flutter.'),
+      ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        ),
+        itemCount: images.length,
+        itemBuilder: (BuildContext context, int index) {
+              File imageFile = File(images[index].path);
+          
+          return InkWell(
+              child: Image.file(imageFile),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => ImageViewScreen(
+                          imageFile: imageFile,
+                        )));
+              });
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: _optionsDialogBox,
+      ),
+    );
+  }
+
+  Future<void> _optionsDialogBox() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Text('image_picker: Cámara'),
+                    onTap: _openCamera,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  GestureDetector(
+                    child:  Text('image_picker: Galería'),
+                    onTap: _openGallery,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  GestureDetector(
+                    child:  Text('camera: cámara'),
+                    onTap: () async {
+                      String picturePath = await Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (context) => CameraScreen()));
+                      Navigator.pop(context);
+                      print(picturePath);
+                      setState(() {
+                        images.add(XFile(picturePath));
+                        //images!.add(PickedFile(picturePath));
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  Future<void>  _openCamera() async {
+    // List<XFile>? images = await _image.pickMultiImage();
+    //PickedFile picture = await imagePicker.getImage(source: ImageSource.camera,);
+    file = await _image!.pickImage(source: ImageSource.camera);
+    Navigator.pop(context);
+    setState(() {
+      images.add(file!);
+    });
+  }
+
+  Future _openGallery() async {
+    // PickedFile picture = await imagePicker.pickImage(source: source)(
+    //   source: ImageSource.gallery,
+    // );
+      file = await _image!.pickImage(source: ImageSource.gallery);
+    Navigator.pop(context);
+    setState(() {
+      images.add(file!);
+      //images;
+    });
+  }
+}
