b714dec7f978d22ad05d0c8cfa018ca805311f98
Jose Alfredo <chpgomezito@gmail.com>
2021-12-17 11:05:29 -0600

    Se agrego el boton de regresar

--- lib/src/pages/image_view.dart
+++ lib/src/pages/image_view.dart
@@ -3,18 +3,27 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
 
-
-class ImageViewScreen extends StatelessWidget {
-  ImageViewScreen({required this.imageFile});
+class ImageViewScreen extends StatefulWidget {
   File imageFile;
+  ImageViewScreen({Key? key, required this.imageFile}) : super(key: key);
+//late File imageFile;
+  @override
+  _ImageViewScreenState createState() => _ImageViewScreenState();
+}
 
+class _ImageViewScreenState extends State<ImageViewScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: PhotoView(
-        imageProvider: FileImage(imageFile),
-        //heroAttributes: "",
+    return Scaffold(
+      body: PhotoView(
+        imageProvider: FileImage(widget.imageFile),
+        heroAttributes: PhotoViewHeroAttributes(tag: "Imagen"),
+      ),
 
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        Navigator.pop(context);
+        
+      },child: Icon(Icons.arrow_back_sharp),
       ),
     );
   }
